08d822b66462fb572191a4a465a1b8f4acb29b1b
YashPatil715 <83406399+YashPatil715@users.noreply.github.com>
2021-05-12 21:21:40 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var platform1;
+var countDistanceX = 0;
+var gap;
+function preload(){
+}
+
+function setup() {
+  createCanvas(9000, 668);
+  for(var i=0;i<6;i++){
+  platform1=new Platform(countDistanceX);
+  gap=random([0,0,0,0,80])
+  countDistanceX=countDistanceX+platform1.rw+gap;
+}
+}
+
+function draw() {
+  background('skyblue'); 
+ drawSprites();
+}
+
